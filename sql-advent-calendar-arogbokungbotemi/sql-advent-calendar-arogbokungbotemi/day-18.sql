36e99c7f79bf9c3ce96ba7c880cc5d2784b97ef0
AroBola <88308549+AroBola@users.noreply.github.com>
2026-01-03 16:34:32 +0100

    Update day-18.sql

--- sql-advent-calendar-arogbokungbotemi/sql-advent-calendar-arogbokungbotemi/day-18.sql
+++ sql-advent-calendar-arogbokungbotemi/sql-advent-calendar-arogbokungbotemi/day-18.sql
@@ -18,8 +18,8 @@
 -- My Solution:
 
 SELECT
-subject,
-FIRST_VALUE (score) OVER (PARTITION BY subject ORDER BY quiz_date) AS first_score,
-LAST_VALUE (score) OVER (PARTITION BY subject ORDER BY quiz_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_score
+  subject,
+  FIRST_VALUE (score) OVER (PARTITION BY subject ORDER BY quiz_date) AS first_score,
+  LAST_VALUE (score) OVER (PARTITION BY subject ORDER BY quiz_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_score
 FROM
 daily_quiz_scores
